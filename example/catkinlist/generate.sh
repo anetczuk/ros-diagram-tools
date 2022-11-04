b3241629fc0566f7a54046d9bc2081e89d891f6e
anetczuk <anetczuk@o2.pl>
2022-11-04 22:06:03 +0100

    Add examples

---
+++ example/catkinlist/generate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+
+## works both under bash and sh
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+
+TOOL_DIR="$SCRIPT_DIR/../../src"
+
+OUT_DIR="$SCRIPT_DIR/out"
+
+
+mkdir -p $OUT_DIR
+
+
+$TOOL_DIR/catkintree.py --file $SCRIPT_DIR/catkin_list.txt --outraw $OUT_DIR/graph.gv.txt --outpng $OUT_DIR/graph.png $@
+
+
+## dot -Tpng graph.gv.txt -o graph2.png
---
+++ example/rostopiclist/generate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+
+## works both under bash and sh
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+
+TOOL_DIR="$SCRIPT_DIR/../../src"
+
+OUT_DIR="$SCRIPT_DIR/out"
+
+
+mkdir -p $OUT_DIR
+
+
+$TOOL_DIR/rostopictree.py --dump_dir $SCRIPT_DIR/info --outraw $OUT_DIR/graph.gv.txt --outpng $OUT_DIR/graph.png $@
+
+
+## dot -Tpng graph.gv.txt -o graph2.png
