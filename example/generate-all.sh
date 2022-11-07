40396da47ee23293ddd22378137324865dc9005a
anetczuk <anetczuk@o2.pl>
2022-11-07 23:27:24 +0100

    Make executable

--- example/generate-all.sh
+++ example/generate-all.sh
@@ -7,11 +7,13 @@ set -eu
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 
+$SCRIPT_DIR/codedistribution/generate.sh
+
 $SCRIPT_DIR/catkinlist/generate.sh
 
 $SCRIPT_DIR/rostopiclist/generate.sh
 
-$SCRIPT_DIR/codedistribution/generate.sh
+$SCRIPT_DIR/rosnodelist/generate.sh
 
 
 ## generate small images
