868aac4cea7e96ce5c9332ce294c3a1443eb94eb
anetczuk <anetczuk@o2.pl>
2022-11-29 21:38:26 +0100

    Rename directory

--- example/rostopiclist/generate.sh
+++ example/rostopiclist/generate.sh
@@ -15,7 +15,7 @@ OUT_DIR="$SCRIPT_DIR/out"
 mkdir -p $OUT_DIR
 
 
-$TOOL_DIR/rostopictree.py --dump_dir $SCRIPT_DIR/info --outraw $OUT_DIR/graph.gv.txt --outpng $OUT_DIR/graph.png $@
+$TOOL_DIR/rostopictree.py --dump_dir $SCRIPT_DIR/dump --outraw $OUT_DIR/graph.gv.txt --outpng $OUT_DIR/graph.png $@
 
 
 ## dot -Tpng graph.gv.txt -o graph2.png
