82c391914ff7db144ea91ce0b0079b35e9e04104
anetczuk <anetczuk@o2.pl>
2022-11-30 23:33:42 +0100

    Update examples

--- example/codedistribution/generate.sh
+++ example/codedistribution/generate.sh
@@ -10,13 +10,17 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 TOOL_DIR="$SCRIPT_DIR/../../src/rosdiagram"
 CHECK_DIR="$TOOL_DIR/.."
 
+DUMP_DIR="$SCRIPT_DIR/dump"
 OUT_DIR="$SCRIPT_DIR/out"
 
 
+mkdir -p $DUMP_DIR
 mkdir -p $OUT_DIR
 
 
-$TOOL_DIR/codedistribution.py --dir $CHECK_DIR --highlight "$SCRIPT_DIR/highlight.txt" --outraw $OUT_DIR/graph.gv.txt --outpng $OUT_DIR/graph.png $@
+$TOOL_DIR/../dump_cloc.py --cloc_dir $CHECK_DIR --out_path $DUMP_DIR/cloc.txt $@
+
+$TOOL_DIR/codedistribution.py --cloc_path $DUMP_DIR/cloc.txt --highlight "$SCRIPT_DIR/highlight.txt" --outraw $OUT_DIR/graph.gv.txt --outpng $OUT_DIR/graph.png $@
 
 
 ## dot -Tpng graph.gv.txt -o graph2.png
