9731890ea7490bb2d006f7f629a8a6cf5a582445
anetczuk <anetczuk@o2.pl>
2023-01-26 23:23:08 +0100

    Update scripts

--- tools/doccheck.sh
+++ tools/doccheck.sh
@@ -8,7 +8,6 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 src_dir=$SCRIPT_DIR/../src
 
 
-disabled_tool() {
 ## D100: Missing docstring in public module
 ## D101: Missing docstring in public class
 ## D102: Missing docstring in public method
@@ -20,7 +19,7 @@ ignore_errors=D100,D101,D102,D103,D104,D105,D107
 
 
 echo "running pydocstyle"
-python -m pydocstyle --count --convention=numpy --add-ignore=$ignore_errors $src_dir
+python3 -m pydocstyle --count --convention=numpy --add-ignore=$ignore_errors $src_dir
 # pydocstyle --count --ignore=$ignore_errors $src_dir
 exit_code=$?
 
@@ -29,4 +28,3 @@ if [ $exit_code -ne 0 ]; then
 fi
 
 echo "pydocstyle -- no warnings found"
-}
