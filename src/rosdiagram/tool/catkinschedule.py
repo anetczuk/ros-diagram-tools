179f0e7b565ca3caadea6ea2dbf2f15dd3f201d1
anetczuk <anetczuk@o2.pl>
2023-02-24 22:02:41 +0100

    Fix static code checks

--- src/rosdiagram/tool/catkinschedule.py
+++ src/rosdiagram/tool/catkinschedule.py
@@ -11,16 +11,13 @@ import argparse
 import re
 import hashlib
 import pprint
-from typing import List
+from typing import List, Dict, Any
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 from PIL import Image, ImageDraw
 
-from rosdiagram.graphviz import Graph, preserve_neighbour_nodes, set_nodes_style,\
-    preserve_top_subgraph
-from rosdiagram.io import read_file, write_file, prepare_filesystem_name
-from rosdiagram.graphviztohtml import generate_graph_html
+from rosdiagram.io import read_file, write_file
 from rosdiagram import texttemplate
 
 
@@ -37,33 +34,33 @@ class Job():
     name: str         = None
     start_time: float = None
     end_time: float   = None
-    
+
     def duration(self):
         return self.end_time - self.start_time
 
 
 class Schedule():
-    
+
     def __init__(self, jobs_list=None):
         self.queues: List[ list ] = []
         if jobs_list is not None:
-            self.parse_list( jobs_list )
+            self.parseList( jobs_list )
 
-    def parse_list( self, jobs_list: List[Job] ):
+    def parseList( self, jobs_list: List[Job] ):
         self.queues.clear()
         for job in jobs_list:
-            self._add_job( job )
+            self._addJob( job )
 
     def queuesNum(self):
         return len( self.queues )
 
-    def jobs_list(self) -> List[Job]:
+    def jobsList(self) -> List[Job]:
         ret_list = []
         for queue in self.queues:
             ret_list.extend( queue )
         return ret_list
 
-    def end_time(self):
+    def endTime(self):
         total_end_time = 0
         for queue in self.queues:
             if not queue:
@@ -73,20 +70,20 @@ class Schedule():
             total_end_time = max( total_end_time, last_end )
         return total_end_time
 
-    def get_critical_path( self ) -> List[Job]:
+    def getCriticalPath( self ) -> List[Job]:
         ret_list = []
         infinity = float("inf")
-        last_job: Job = self.get_before( infinity )
+        last_job: Job = self.getBefore( infinity )
         while True:
             if last_job is None:
                 break
             ret_list.append( last_job )
             start_time = last_job.start_time
-            last_job: Job = self.get_before( start_time )
+            last_job = self.getBefore( start_time )
         ret_list.reverse()
         return ret_list
-    
-    def get_before( self, end_time, ref_job=None ) -> Job:
+
+    def getBefore( self, end_time, ref_job=None ) -> Job:
         last_job = None
         for queue in self.queues:
             for job in queue:
@@ -102,7 +99,7 @@ class Schedule():
                     last_job = job
         return last_job
 
-    def _add_job( self, job: Job ):
+    def _addJob( self, job: Job ):
         curr_start_time = job.start_time
         for queue in self.queues:
             last_job = queue[ -1 ]
@@ -127,29 +124,29 @@ def generate_pages( schedule: Schedule, out_dir, config_params_dict=None ):
     generate_plant_graph( schedule, graph_path )
     # generate_dot_graph( schedule )
 
-    params_dict = {}
+    params_dict: Dict[ str, Any ] = {}
     generate_graph_page( schedule, params_dict, out_dir )
 
 
 def generate_graph_page( schedule: Schedule, item_config_dict, output_dir ):
     page_params = item_config_dict.copy()
 
-    build_time = schedule.end_time()
+    build_time = schedule.endTime()
     total_time = print_time( build_time, False )
 
-    critical_path = schedule.get_critical_path()
+    critical_path = schedule.getCriticalPath()
     critical_path = print_critical_path( schedule, critical_path )
 
-    packages_list = schedule.jobs_list()
+    packages_list = schedule.jobsList()
     dur_list      = print_durations( packages_list, True )
 
     packages_list = print_durations( packages_list, False )
     packages_list.sort()
- 
+
     ## prepare input for template
     page_params.update( {   "body_color":   "#bbbbbb",
                             "svg_name":     "schedule.svg",
-#                             "main_page_link":   main_page_link,
+                            # "main_page_link":   main_page_link,
 
                             "total_time": total_time,
                             "critical_path": critical_path,
@@ -161,7 +158,7 @@ def generate_graph_page( schedule: Schedule, item_config_dict, output_dir ):
     main_out_path = os.path.join( output_dir, "full_graph.html" )
 
     texttemplate.generate( template_path, main_out_path, INPUT_DICT=page_params )
-    
+
     _LOGGER.info( "writing main page: file://%s", main_out_path )
 
 
@@ -170,12 +167,13 @@ def generate_graph_page( schedule: Schedule, item_config_dict, output_dir ):
 
 def read_build_log( log_path ) -> List[Job]:
     content = read_file( log_path )
-    
+
     order_list = []
     start_dict = {}
     end_dict   = {}
-    last_end_time   = 0
-    build_timestamp = 0         ## sometimes catkin not starts next job exactly after one finishes, but waits some time
+    last_end_time   = 0.0
+    ## sometimes catkin not starts next job exactly after one finishes, but waits some time
+    build_timestamp = 0.0
 
     for line in content.splitlines():
         line = line.strip()
@@ -198,7 +196,7 @@ def read_build_log( log_path ) -> List[Job]:
 
         line_log = get_after( line, "Finished <<< " )
         if line_log:
-            split_content = re.split( "\[|]", line_log )
+            split_content = re.split( r"\[|]", line_log )
             package_name = split_content[0]
             package_name = package_name.strip()
             time_line = split_content[1]
@@ -234,14 +232,14 @@ def read_build_log( log_path ) -> List[Job]:
         #print( ">", item, "<" )
         jobs_list.append( item )
         #print( ">", item, "<" )
-    
+
     return jobs_list
 
 
 def get_after( content, start ):
     try:
         pos = content.index( start )
-        return content[ pos + len(start) : ]
+        return content[ pos + len(start): ]
     except ValueError:
         pass
     return None
@@ -273,7 +271,7 @@ def get_build_timestamp( content ):
 
 def draw_shedule( schedule: Schedule, out_path ):
     queues_num = schedule.queuesNum()
-    total_end_time = schedule.end_time()
+    total_end_time = schedule.endTime()
     total_end_time = int( total_end_time )
 
     pprint.pprint( schedule )
@@ -284,12 +282,12 @@ def draw_shedule( schedule: Schedule, out_path ):
     margin      = 10
 
     ## init canvas
-    canvas = ( total_end_time + 2 * margin, 
+    canvas = ( total_end_time + 2 * margin,
                queues_num * ( margin + rect_height ) + margin
                )
     im = Image.new('RGBA', canvas, (255, 255, 255, 255))
     draw = ImageDraw.Draw( im )
-    
+
     for q in range(0, queues_num):
         queue = schedule.queues[ q ]
         queue_x = margin
@@ -306,13 +304,13 @@ def draw_shedule( schedule: Schedule, out_path ):
             color_int = int( item_hash, 16 )
             color     = rgb_from_int( color_int )
             draw.rectangle( rect_coords, fill=color, outline=(0, 0, 0, 255) )
-    
+
     im.save( out_path )
 
 
 def rgb_from_int( color_num ):
-    color_int = color_num % (256*256*256)
-    blue  =  color_int & 255
+    color_int = color_num % (256 * 256 * 256)
+    blue  = color_int & 255
     green = (color_int >> 8) & 255
     red   = (color_int >> 16) & 255
     return (red, green, blue)
@@ -331,29 +329,28 @@ def print_durations( jobs_list: List[Job], sort_list=False ):
         items.sort( key=lambda item: item[1], reverse=True )
 
     ret_list = []
-    for job in items:
-        time_text = print_time( job[1] )
-        ret_list.append( ( job[0], time_text ) )
+    for item in items:
+        time_text = print_time( item[1] )
+        ret_list.append( ( item[0], time_text ) )
     return ret_list
 
 
-def print_time( time_value, leading_zeros = True ):
+def print_time( time_value, leading_zeros=True ):
     dur  = time_value
     mins = int( dur / 60 )
     secs = dur % 60.0
     secs = round( secs, 2 )
-    if leading_zeros:
-        return f"{mins:0>3} m {secs:0>4} s"
-    else:
+    if not leading_zeros:
         return f"{mins} m {secs:0>4} s"
+    return f"{mins:0>3} m {secs:0>4} s"
 
 
 def print_critical_path( schedule: Schedule, jobs_list: List[Job], sort_list=False ):
     items = []
     for job in jobs_list:
         duration = job.duration()
-        gap_time = 0
-        prev_job = schedule.get_before( job.end_time, job )
+        gap_time = 0.0
+        prev_job = schedule.getBefore( job.end_time, job )
         if prev_job:
             gap_time = job.end_time - prev_job.end_time
         else:
@@ -365,10 +362,10 @@ def print_critical_path( schedule: Schedule, jobs_list: List[Job], sort_list=Fal
         items.sort( key=lambda item: item[1], reverse=True )
 
     ret_list = []
-    for job in items:
-        duration = format_duration( job[1] )
-        gap      = format_duration( job[2] )
-        ret_list.append( ( job[0], duration, gap, job[3] ) )
+    for item in items:
+        duration = format_duration( item[1] )
+        gap      = format_duration( item[2] )
+        ret_list.append( ( item[0], duration, gap, item[3] ) )
     return ret_list
 
 
@@ -389,14 +386,14 @@ scale 20 as 100 pixels
 
 """
 
-    critical_path = schedule.get_critical_path()
+    critical_path = schedule.getCriticalPath()
 
     queues_num = len( schedule.queues )
     for q in range(0, queues_num):
         content += f"concise \"thread {q}\" as queue{q}\n"
-     
+
     content += "\n"
-    
+
     for q in range(0, queues_num):
         content += f"@queue{q}\n"
         queue = schedule.queues[ q ]
@@ -426,14 +423,14 @@ scale 20 as 100 pixels
             content += f"{curr_start_time} is \"{curr_job.name}\" {color}\n"
             curr_end_time = int( curr_job.end_time )
             if j != queue_size - 1:
-                next_job = queue[j+1]
+                next_job = queue[j + 1]
                 next_start_time = int( next_job.start_time )
-                if next_start_time > curr_end_time: 
+                if next_start_time > curr_end_time:
                     content += f"{curr_end_time} is {{hidden}}\n"
             else:
                 ## last element -- add hidden
                 content += f"{curr_end_time} is {{hidden}}\n"
-        
+
         content += "\n"
 
     content += "@enduml\n"
@@ -446,14 +443,14 @@ scale 20 as 100 pixels
 #     base_graph = graph.base_graph
 #     base_graph.set_type( 'digraph' )
 #     base_graph.set_rankdir( 'LR' )
-# 
+#
 #     for queue in schedule:
 #         for job in queue:
 #             node = graph.addNode( job[0], shape="box" )
 #             duration = int( (job[2] - job[1]) / 2 )
 #             node.set( "width", duration )
 #             node.set( "fixedsize", "true" )
-#             
+#
 #         ## add edges between jobs in queue
 #         queue_size = len(queue)
 #         if queue_size > 1:
@@ -464,7 +461,7 @@ scale 20 as 100 pixels
 #                 edge = graph.addEdge( curr_job[0], next_job[0] )
 #                 if edge is not None:
 #                     edge.set( "weight", 9999 )
-# 
+#
 #     ## add connections between queues
 #     for queue in schedule:
 #         for job in queue:
@@ -479,7 +476,7 @@ scale 20 as 100 pixels
 # #                         edge = graph.addEdge( job[0], other_job[0] )
 # #                         if edge is not None:
 # #                             edge.set( "weight", 9999 )
-# 
+#
 #     graph.writeRAW( "schedule.gv.txt" )
 #     graph.writePNG( "schedule.gv.png" )
 #     graph.write( "schedule.gv.svg", file_format='svg')
