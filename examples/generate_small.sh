5c351d25618d63dd6c49a2132992c9fec0525b5d
anetczuk <anetczuk@o2.pl>
2023-05-18 19:50:04 +0200

    Resize generated images

--- examples/generate_small.sh
+++ examples/generate_small.sh
@@ -6,20 +6,5 @@ set -eu
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 
-big_suffix="-big."
-small_suffix="-small."
-
-for filename in $(find $SCRIPT_DIR -name "*.png"); do
-    if [[ $filename == *"$small_suffix"* ]]; then
-        continue
-    fi
-    small_name=${filename/".png"/"${small_suffix}png"}
-
-    ##if [[ $filename != *"$big_suffix"* ]]; then
-    ##    continue
-    ##fi
-    ##small_name=${filename/$big_suffix/$small_suffix}
-
-    echo "converting: $filename -> $small_name"
-    convert $filename -strip -resize 400 $small_name
-done
+## 'source' is somehow simillar to C 'include'
+source $SCRIPT_DIR/../doc/generate_small.sh
