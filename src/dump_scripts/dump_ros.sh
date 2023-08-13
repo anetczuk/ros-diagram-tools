60c42db7e6409ca1002d3340a013640dfb2e55f3
anetczuk <anetczuk@o2.pl>
2023-08-14 00:38:59 +0200

    Add comments

--- src/dump_scripts/dump_ros.sh
+++ src/dump_scripts/dump_ros.sh
@@ -29,8 +29,9 @@ INFO_DIR="$1"
 mkdir -p $INFO_DIR
 
 
-echo "executing: $SCRIPT_DIR/dump_rosparam.sh $INFO_DIR/paraminfo"
-$SCRIPT_DIR/dump_rosparam.sh "$INFO_DIR/paraminfo"
+## $SCRIPT_DIR/dump_catkindeps.sh runs on catkin workspace not on running roscore
+
+## $SCRIPT_DIR/dump_clocdir.sh runs on source directory
 
 echo "executing: $SCRIPT_DIR/dump_rospack.sh $INFO_DIR/packinfo"
 $SCRIPT_DIR/dump_rospack.sh "$INFO_DIR/packinfo"
@@ -38,11 +39,16 @@ $SCRIPT_DIR/dump_rospack.sh "$INFO_DIR/packinfo"
 echo "executing: $SCRIPT_DIR/dump_clocpack.sh $INFO_DIR/clocpack"
 $SCRIPT_DIR/dump_clocpack.sh "$INFO_DIR/packinfo/list.txt" "$INFO_DIR/clocpack"
 
+## $SCRIPT_DIR/dump_roslaunch.sh requires to pass launch file
+
+echo "executing: $SCRIPT_DIR/dump_rosmsg.sh $INFO_DIR/msginfo"
+$SCRIPT_DIR/dump_rosmsg.sh "$INFO_DIR/msginfo"
+
 echo "executing: $SCRIPT_DIR/dump_rosnode.sh $INFO_DIR/nodeinfo"
 $SCRIPT_DIR/dump_rosnode.sh "$INFO_DIR/nodeinfo"
 
-echo "executing: $SCRIPT_DIR/dump_rostopic.sh $INFO_DIR/topicinfo"
-$SCRIPT_DIR/dump_rostopic.sh "$INFO_DIR/topicinfo"
+echo "executing: $SCRIPT_DIR/dump_rosparam.sh $INFO_DIR/paraminfo"
+$SCRIPT_DIR/dump_rosparam.sh "$INFO_DIR/paraminfo"
 
 echo "executing: $SCRIPT_DIR/dump_rosservice.sh $INFO_DIR/serviceinfo"
 $SCRIPT_DIR/dump_rosservice.sh "$INFO_DIR/serviceinfo"
@@ -50,9 +56,7 @@ $SCRIPT_DIR/dump_rosservice.sh "$INFO_DIR/serviceinfo"
 echo "executing: $SCRIPT_DIR/dump_rossrv.sh $INFO_DIR/srvinfo"
 $SCRIPT_DIR/dump_rossrv.sh "$INFO_DIR/srvinfo"
 
-echo "executing: $SCRIPT_DIR/dump_rosmsg.sh $INFO_DIR/msginfo"
-$SCRIPT_DIR/dump_rosmsg.sh "$INFO_DIR/msginfo"
-
-## $SCRIPT_DIR/dump_roslaunch.sh requires to pass launch file
+echo "executing: $SCRIPT_DIR/dump_rostopic.sh $INFO_DIR/topicinfo"
+$SCRIPT_DIR/dump_rostopic.sh "$INFO_DIR/topicinfo"
 
 echo -e "\nDone."
