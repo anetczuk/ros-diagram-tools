82a2c100ee0ce8baae428dd0105f019ad989e892
anetczuk <anetczuk@o2.pl>
2023-10-26 23:24:49 +0200

    Refactor templates: extract common functions

---
+++ src/rosdiagram/template/utils_html.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2022, Arkadiusz Netczuk <dev.arnet@gmail.com>
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+def code_inline( text ):
+    return f"<code>{text}</code>"
+
+
+def link_simple( label, url ):
+    return f"""<a href="{url}">{label}</a>"""
